ce580a5781304fa651cb2e39579ccd1bb186c184
Birkti Girma Ayele <60798849+Birktiayele@users.noreply.github.com>
2023-09-30 18:00:48 -0500

    Update ConnectFour.java

--- ConnectFour/src/ConnectFour.java
+++ ConnectFour/src/ConnectFour.java
@@ -1,9 +1,10 @@
-/*Birkti Ayele
- *3/3/2021
- *ConnectFour is a game that can be played by two players. In this game the first player is always 'Y' 
- *and the second player is 'R'. The players turn by turn drop a disk into a seven-column six-row vertically
- *suspended board. The program redisplays the board and show the status of the game (win, draw, or continue).
- * 
+/*
+ * ConnectFour.java
+ * Birkti Ayele
+ * Date: 3/3/2021
+ * Description: ConnectFour is a two-player game where one player is 'Y' and the other is 'R'. Players take turns
+ * dropping disks into a vertically suspended board with seven columns and six rows. The program displays the
+ * current board and game status (win, draw, or continue).
  */
 import java.util.Scanner;
 
@@ -12,14 +13,16 @@ public class ConnectFour {
     static final int ROWS = 6, COLS = 7;
     static char[][] board = new char[ROWS][COLS]; // 6-x-7 array to simulate the board
     public static void main(String[] args) {
-        // declare variables
-        int turn = 1; // turn starts from one
-        char player = 'Y'; // yellow goes first
-        boolean winner = false;
-        int game = 0;
+         // Initialize variables
+         int turn = 1; // Turn starts from one
+         char player = 'Y'; // Yellow goes first
+         boolean winner = false;
+         int game = 0;
+
         do {
-            displayBoard(board); // display the board
-            // play a turn
+            displayBoard(board); // Display the board
+
+            // Play a turn
             while (!winner && turn <= 42) {
                 boolean place;
                 int column;
@@ -28,12 +31,14 @@ public class ConnectFour {
                     place = dropDisk(board, column, player);
                     displayBoard(board);
                 } while (!place);
-                if(isWinner(board)) // determine the winner
+
+                if(isWinner(board)) // Determine the winner
                 {
                     winner = true;
                     break;
                 }
-                // switch players
+
+                // Switch players
                 if (player == 'R') {
                     player = 'Y';
                 } else {
@@ -41,7 +46,8 @@ public class ConnectFour {
                 }
                 turn++; //Update total disks in the board
             }
-            // display the winner or declare a draw
+
+            // Display the winner or declare a draw
             if (winner) {
                 if (player == 'R') {
                     System.out.println("R has WON the game!");
@@ -52,31 +58,34 @@ public class ConnectFour {
                 System.out.println("I declare a draw");
             }
 
-            // We get here if the game is done, now we check if user wants to play again
-            // prompt user to type 1 if they want to continue the game
+            // Check if the user wants to play again
             System.out.println("DO YOU WANT TO PLAY A NEW GAME? (type 1 for yes)");
             game = input.nextInt();
+
             // Reset game regardless of input
-            clearBoard(board); // clear the board and restart the game
+            clearBoard(board); // Clear the board and restart the game
             winner = false;
         } while (game == 1);
     }
+
+    // Display the current game board
     private static void displayBoard(char[][] board) {
-        // This method displays the current board, so the user can see what's
-        // currently where, and they can plan their next move accordingly
-        //
         for (int i = board.length - 1; i >= 0; i--) {
-            System.out.print("| ");
+            System.out.print("|");
             for (int j = 0; j < board[i].length; j++) {
-                System.out.print(board[i][j] + " | ");
+                char disc = board[i][j];
+                if (disc == '\u0000') {
+                    System.out.print("   |"); 
+                } else {
+                    System.out.print(" " + disc + " |"); // Place 'Y' or 'R' with spacing
+                }
             }
-            System.out.println(); // newline
+            System.out.println(); // Newline
         }
-    }// displayBoard
+    }
+
+    // Check if a disk can be dropped into the specified column
     public static boolean dropDisk(char[][] board, int column, char player) {
-        // This method checks if the column is invalid or if the column is full.
-        // if the column is invalid or full it asks for another input
-        //
         for (int i = 0; i < board.length; i++)
             while (column < 0 || column > 6 || board[5][column] != '\u0000') {
                 System.out.println("YOU ENTERED AN INVALID INTEGER OR THE COLUMN IS FULL.");
@@ -85,6 +94,8 @@ public class ConnectFour {
         boolean x = placeADisk(board, column, player);
         return x;
     }
+
+    // Place a disk in the specified column
     public static boolean placeADisk(char[][] board, int column, char player) {
         for (int i = 0; i < ROWS; i++) {
             if (board[i][column] == '\u0000') {
@@ -95,7 +106,8 @@ public class ConnectFour {
         return false;
     }
     public static boolean isWinner(char[][] board) {
-        // this method checks the board to see if there are four-in-a-row in either the
+        
+        // This method checks the board to see if there are four-in-a-row in either the
         // vertical, horizontal, or
         // the two diagonal directions. It starts by going through all combinations of
         // row and column that can
@@ -116,23 +128,22 @@ public class ConnectFour {
         // |*|*|*|*| | | | possibly GET four-in-a-row!
         // |*|*|*|*| | | |
         // |*|*|*|*| | | |
-        //
-        //
-        for (int r = 0; r < ROWS; r++) { // for EVERY row
-            for (int c = 0; c <= 3; c++) { // for columns 0-3
-                if (board[r][c] != '\u0000' && // if it's not empty, AND
-                        board[r][c] == board[r][c + 1] && // it matches neighbor 1 AND
-                        board[r][c] == board[r][c + 2] && // it matches TWO over AND
+        
+        for (int r = 0; r < ROWS; r++) { // For EVERY row
+            for (int c = 0; c <= 3; c++) { // For columns 0-3
+                if (board[r][c] != '\u0000' && // If it's not empty, AND
+                        board[r][c] == board[r][c + 1] && // It matches neighbor 1 AND
+                        board[r][c] == board[r][c + 2] && // It matches TWO over AND
                         board[r][c] == board[r][c + 3]) {
                     return true; // THREE over, then it's a WINNER!
                 }
             }
         }
-        for (int r = 0; r < 3; r++) { // for every row 0-3
-            for (int c = 0; c < COLS; c++) { // for EVERY col
-                if (board[r][c] != '\u0000' && // if it's not empty, AND
-                        board[r][c] == board[r + 1][c] && // it matches neighbor 1 AND
-                        board[r][c] == board[r + 2][c] && // it matches TWO over AND
+        for (int r = 0; r < 3; r++) { // For every row 0-3
+            for (int c = 0; c < COLS; c++) { // For EVERY col
+                if (board[r][c] != '\u0000' && // If it's not empty, AND
+                        board[r][c] == board[r + 1][c] && // It matches neighbor 1 AND
+                        board[r][c] == board[r + 2][c] && // It matches TWO over AND
                         board[r][c] == board[r + 3][c]) {
                     return true; // THREE over, then it's a WINNER!
                 }
@@ -154,17 +165,16 @@ public class ConnectFour {
                 }
             }
         }
-        return false; // If we haven't hit any of the above cases, then we don't have a winner
+        return false; // If no winner is found
     }
+
+    // Clear the game board to start a new game
     public static void clearBoard(char[][] board) {
-        // This method clears the board and restart a new game
         for (int i = 0; i < ROWS; i++) {
             for (int j = 0; j < COLS; j++) {
-                board[i][j] = '\u0000'; // set every row/col to 'u/0000' like it was initially
+                board[i][j] = '\u0000'; // Reset every row/column to '\u0000'
             }
         }
     }
 
 }
-
-
